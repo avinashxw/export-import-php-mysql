928be34b43cfde2dd4ed11c6fa29f3cd9a50c8e2
Avinash Wadhawan <avinashxw@gmail.com>
2021-05-28 23:57:58 +0530

    title-change

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.24/datatables.min.css"/>
-    <title>Export Data</title>
+    <title>Export Data using PHP & MySQL</title>
   </head>
   <body>
     <h1>Hello, world!</h1>
